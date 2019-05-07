25b442cf065c951f7d09e7a7c7cd7d475615c56a
Steve Lanier <stevekentlanier@gmail.com>
2019-05-07 15:35:37 -0700

    Day 2 MVP complete

---
+++ auth/auth-router.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+
+// for endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username; // <<<<<<<<<<<<<<<<<<<<<<<
+        // the cookie is sent by the express-session library
+        res.status(200).json({
+          message: `Welcome ${user.username}, have a cookie!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send(
+          'you can check out any time you like, but you can never leave....'
+        );
+      } else {
+        res.send('bye');
+      }
+    });
+  } else {
+    res.send('already logged out');
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const Users = require('./data/dbHelpers')
+const server = require('./api/server.js');
 
-const server = express()
-
-server.use(express.json())
-
-server.get('/', (req, res) => {
-    res.send('testing')
-})
-
-server.post('/api/register', (req, res) => {
-    const user = req.body
-    user.password = bcrypt.hashSync(user.password, 10)
-    console.log(user)
-    Users.addUser(user)
-        .then(resp => res.json(resp))
-        .catch(err => res.json(err))
-})
-
-server.get('/api/users', (req, res) => {
-    Users.find()
-        .then(response => res.json(response))
-        .catch(err => res.json(err))
-});
-
-server.post('/api/login', (req, res) => {
-    const {username, password} = req.body
-
-    Users.findBy({username: username})
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                res.json({message: `Welcome ${user.username}`})
-            } else res.json({err: 'You shall not pass'})
-        })
-        .catch(err => res.json(err))
-
-})
-
-server.listen(3300, () => console.log('Server up'))
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
